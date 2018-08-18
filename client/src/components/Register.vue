2c2f2b14da5e11c2481ae9b83fa7f0ef71397103
unknown <sachinsinghsindhu@gmail.com>
2018-08-18 14:50:11 +0530

    register page /api added

---
+++ client/src/components/Register.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <h1>Register</h1>
+    <br>
+    <input
+      type="email"
+      name="email"
+      placeholder="email"
+      v-model="email"
+    >
+    <br>
+    <input
+      type="password"
+      name="password"
+      placeholder="password"
+      v-model="password"
+    >
+    <br>
+    <button @click="register">Register</button>
+  </div>
+</template>
+
+<script>
+import AuthenticationService from '@/services/AuthenticationService';
+
+export default {
+  data() {
+    return {
+      email: '',
+      password: '',
+    };
+  },
+  methods: {
+    async register() {
+      const res = await AuthenticationService.register({
+        email: this.email,
+        password: this.password,
+      });
+      console.log(res.data);
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
